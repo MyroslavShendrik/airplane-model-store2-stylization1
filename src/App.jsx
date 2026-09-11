36e0c53fbe9b828b454503dbd91795adec3a4047
MyroslavShendrik <citrus.user2204@gmail.com>
2026-09-11 19:59:18 +0300

    lesson-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Section from './components/SectionWOStyle';  //! без стилізації
-// import Section from './components/SectionBuiltInStyles'; //! вбудовані стилі
+// import Section from './components/SectionWOStyle';  //! без стилізації
+import Section from './components/SectionBuiltInStyles'; //! вбудовані стилі
 
 import PlanesList from './components/PlanesListWOStyle';  //! без стилізації
 import planes from './json/planes.json';
