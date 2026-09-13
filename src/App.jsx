ab9dd5f6e3f2e61bfa4e56830688be93ba9626ef
MyroslavShendrik <citrus.user2204@gmail.com>
2026-09-13 13:11:01 +0300

    lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 // import Section from './components/SectionWOStyle';  //! без стилізації
 import Section from './components/SectionBuiltInStyles'; //! вбудовані стилі
 
-import PlanesList from './components/PlanesListWOStyle';  //! без стилізації
+// import PlanesList from './components/PlanesListWOStyle';  //! без стилізації
+import PlanesList from './components/PlanesListBuiltInStyles.jsx';  //! вбудовані стилі
 import planes from './json/planes.json';
 
 
--- src/components/PlanesListBuiltInStyles.jsx
+++ src/components/PlanesListBuiltInStyles.jsx
@@ -1,36 +1,46 @@
-import PropTypes from 'prop-types';
-import PlaneItem from './PlaneItemBuiltInStyles.jsx'; //! вбудовані стилі
+import PropTypes from "prop-types";
+import PlaneItem from "./PlaneItemBuiltInStyles.jsx"; //! вбудовані стилі
 
 function PlanesList({ items }) {
-    return (
-        <ul>
-            {items.map(item =>
-                <li key={item.id}>
-                    <PlaneItem
-                        urlMain={item.url.main}
-                        urlPromotional={item.url.promotional}
-                        urlActual={item.url.actual} 
-                        nameBrief={item.name.brief}
-                        nameFull={item.name.full}
-                        nickname={item.name.nickname}
-                        year={item.info.year}
-                        country={item.info.country}
-                        type={item.info.type}
-                        price={item.info.price}
-                        description={item.info.description}
-                    />
-                </li>
-            )}
-        </ul>
-    );
-};
+  return (
+    <ul
+      style={{
+        marginLeft: 10,
+        marginRight: 10,
+        padding: 10,
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(445px, 1fr))",
+        gap: 32,
+        outline: "1px solid red",
+      }}
+    >
+      {items.map((item) => (
+        <li key={item.id}>
+          <PlaneItem
+            urlMain={item.url.main}
+            urlPromotional={item.url.promotional}
+            urlActual={item.url.actual}
+            nameBrief={item.name.brief}
+            nameFull={item.name.full}
+            nickname={item.name.nickname}
+            year={item.info.year}
+            country={item.info.country}
+            type={item.info.type}
+            price={item.info.price}
+            description={item.info.description}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 PlanesList.propTypes = {
-    items: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.string.isRequired,
-        }),
-    ),
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  ),
 };
 
 export default PlanesList;
